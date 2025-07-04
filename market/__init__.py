5de52a1eacac0ef462a1bcf92f56804909497c1a
GhandouR <basilelghandour@gmail.com>
2025-07-04 13:53:38 +0300

    Done with Registration

--- market/__init__.py
+++ market/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
 app = Flask(__name__)
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 app.config['SECRET_KEY'] = '30867c65495ecc45f74fec30'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 from market import routes
 
--- market/forms.py
+++ market/forms.py
@@ -1,7 +1,21 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Email, EqualTo, Length, ValidationError
+from wtforms.validators import DataRequired, Email, EqualTo, Length, ValidationError  
+from market.models import User  
 class RegisterForm(FlaskForm):
+    
+    
+    def validate_username(self, username_to_check):
+        user = User.query.filter_by(username=username_to_check.data).first()
+        if user:
+            raise ValidationError('Username already exists. Please try a different one.')
+        
+    def validate_email(self, email_to_check):
+        email = User.query.filter_by(email_address=email_to_check.data).first()
+        if email:
+            raise ValidationError('Email already exists. Please try a different one.')
+        
+            
     username = StringField(label='Username' , validators=[DataRequired(message='Username is Required'), Length(min=3, max=30)])
     email = StringField(label='Email' , validators=[DataRequired(message='Please Enter Your Email'), Email(message='Invalid Email Address')])
     password1 = PasswordField(label='Password' , validators=[DataRequired(message='Enter Your Password'), Length(min=6, max=25)])
